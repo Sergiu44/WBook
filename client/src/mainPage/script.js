41cca8781b87652e8e9b6524b4359b89d8e8233a
Sergiu-Nicolas Stanciu <sergiu.stanciu@ivfuture.uk>
2022-05-06 23:47:29 +0300

    Adding more stylization and functionality for dropdown element

---
+++ client/src/mainPage/script.js
@@ -0,0 +1,40 @@
+function init() {
+    document.querySelectorAll('.seeMore').forEach(element => {
+        element.addEventListener('click', e => {
+            e.preventDefault();
+            const text = element.previousSibling.previousSibling;
+            console.log(text);
+            text.style.textOverflow = 'unset';
+            text.style.overflow = 'unset';
+            text.style.whiteSpace = 'unset';
+            element.style.display = 'none'
+        })
+    })
+
+    const listItems = document.querySelectorAll('.navigation ul li');
+    for(let i = 0; i< listItems.length; ++i) {
+        listItems[i].addEventListener('click', () => {
+            window.scrollTo({
+                top: i===0 ? 0 : (350*(i+1) + (i===listItems.length-1 ? 300 : 0)),
+                behavior: "smooth"
+            });
+        })
+    }
+}
+
+function appearElement() {
+    let elements = document.querySelectorAll(".display");
+    for (const element of elements) {
+        let windowHeight = window.innerHeight;
+        let elementTop = element.getBoundingClientRect().top;
+        let elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            element.classList.add("active");
+        } else {
+            element.classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener('load', init);
+window.addEventListener('scroll', appearElement);
